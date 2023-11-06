980cb10fc59dd6b8f7d89c66a5d05dad823d558e
Saamuuee <samsameliak@gmail.com>
2023-11-06 16:52:05 +0100

    passSimpleTest

--- shell.c
+++ shell.c
@@ -1,5 +1,8 @@
 #include "parser/ast.h"
 #include "shell.h"
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void initialize(void)
 {
@@ -11,8 +14,39 @@ void initialize(void)
 void run_command(node_t *node)
 {
     /* Print parsed input for testing - comment this when running the tests! */
-    print_tree(node);
+    //print_tree(node);
+
+    if (node->type == NODE_COMMAND) {
+        char *program = node->command.program;
+        char **argv = node->command.argv;
+        
+        // Fork a child process
+        pid_t pid = fork();
+        
+        if (pid == 0) {
+            // Child process
+            // Execute the command
+            execvp(program, argv);
+            // If execvp returns, it must have failed
+            fprintf(stderr, "Unknown command or execution failed\n");
+            exit(EXIT_FAILURE);
+        } else if (pid > 0) {
+            // Parent process
+            // Wait for the child process to complete
+            int status;
+            waitpid(pid, &status, 0);
+            if (WIFEXITED(status)) {
+                // You can use the exit status of the child if needed
+            }
+        } else {
+            // Fork failed
+            fprintf(stderr, "Failed to fork process\n");
+        }
+    }
+
+    // ... handle other node types like NODE_PIPE, NODE_REDIRECT, etc.
 
     if (prompt)
         prompt = "vush$ ";
 }
+
